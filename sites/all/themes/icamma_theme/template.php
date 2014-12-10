6f6cbf6e02fae77c975a342071eb22f7f8253fc7
Sharanc25 <Sharanc25@gmail.com>
2014-12-10 20:30:22 +0530

    Changes Login/Register to Login / Create an account
    Removes link to user profile

--- sites/all/themes/icamma_theme/template.php
+++ sites/all/themes/icamma_theme/template.php
@@ -6,8 +6,23 @@
  * 
  */
 
+/**
+* Custom theme function for the login/register link.
+* Change "Register" to create an account
+*/
+function icamma_theme_lt_login_link($variables) {
+// Only display register text if registration is allowed.
+	if (variable_get('user_register', USER_REGISTER_VISITORS_ADMINISTRATIVE_APPROVAL)) {
+		return t('Log in / Create an account');
+	}
+	else {
+		return t('Log in');
+	}
+} 
+
+
 // Convert username to link in logged in users.
-function icamma_theme_lt_loggedinblock(){
+/*function icamma_theme_lt_loggedinblock(){
   global $user;
   return l(check_plain($user->name), 'user/' . $user->uid) .' | ' . l(t('Log out'), 'user/logout');
-}
+}*/
